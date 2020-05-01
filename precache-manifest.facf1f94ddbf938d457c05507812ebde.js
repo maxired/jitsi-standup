fac68a92f1cfa40524a1ddcaa1b6fb3a3dcef8de
Maxence Dalmais <github@maxired.fr>
2020-05-01 17:20:16 +0200

    Updates

---
+++ precache-manifest.facf1f94ddbf938d457c05507812ebde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2a9a25550d81e2463610fa3928df73",
+    "url": "/jitsi-standup/index.html"
+  },
+  {
+    "revision": "c07ed6819c9fcdbcc08c",
+    "url": "/jitsi-standup/static/css/main.aeb1d389.chunk.css"
+  },
+  {
+    "revision": "5472d800b3374a0aea48",
+    "url": "/jitsi-standup/static/js/2.4d3f545c.chunk.js"
+  },
+  {
+    "revision": "640aa39c32c9bb605adf655ed1141834",
+    "url": "/jitsi-standup/static/js/2.4d3f545c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07ed6819c9fcdbcc08c",
+    "url": "/jitsi-standup/static/js/main.bee0eb3c.chunk.js"
+  },
+  {
+    "revision": "502de419e76c61f3ead9",
+    "url": "/jitsi-standup/static/js/runtime-main.cdeadf75.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ac8ecf4f40f88d84d0258f52a54d858.js"
+  "/jitsi-standup/precache-manifest.facf1f94ddbf938d457c05507812ebde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jitsi-standup/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
